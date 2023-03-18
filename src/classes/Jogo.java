3f1cbc4373b6744a3a4354042b6e632e9429151b
karol marques <karoldm1579@gmail.com>
2023-03-18 18:39:59 -0300

    fix: posição relativa ao peao

--- src/classes/Jogo.java
+++ src/classes/Jogo.java
@@ -1,33 +1,35 @@
-
 package classes;
 
 import java.util.ArrayList;
-import java.util.Scanner;   
+import java.util.Random;
+import java.util.Scanner;
+import java.util.random.RandomGenerator;
 
 /**
  *
  * @author Karol
  */
 public class Jogo {
-    private ArrayList<Peao> peoes = new ArrayList<>(); 
+
+    private ArrayList<Peao> peoes = new ArrayList<>();
     private ArrayList<Square> tabuleiro = new ArrayList<>();
-    
-    public Jogo(){
+
+    public Jogo() {
         //iniciando arrays de peoes 
         //4 peoes para cada jogador
-        
-        this.peoes.add(new Peao((byte)0, 0));
-        this.peoes.add(new Peao((byte)0, 0));
-        this.peoes.add(new Peao((byte)0, 0));
-        this.peoes.add(new Peao((byte)0, 0));
-        
-        this.peoes.add(new Peao((byte)1, 14));
-        this.peoes.add(new Peao((byte)1, 14));
-        this.peoes.add(new Peao((byte)1, 14));
-        this.peoes.add(new Peao((byte)1, 14));
+
+        this.peoes.add(new Peao((byte) 0, 0));
+        this.peoes.add(new Peao((byte) 0, 0));
+        this.peoes.add(new Peao((byte) 0, 0));
+        this.peoes.add(new Peao((byte) 0, 0));
+
+        this.peoes.add(new Peao((byte) 1, 14));
+        this.peoes.add(new Peao((byte) 1, 14));
+        this.peoes.add(new Peao((byte) 1, 14));
+        this.peoes.add(new Peao((byte) 1, 14));
 
         //Iniciando tabuleiro
-        for(int i = 0; i < 51; i++){
+        for (int i = 0; i < 51; i++) {
             tabuleiro.add(new Square(null));
         }
 
@@ -42,35 +44,37 @@ public class Jogo {
         tabuleiro.get(14).setPeao(peoes.get(7));
 
     }
-    
+
     //retorna um numero aleatório entre 1 e 6 para simular o dado D6
-    public int jogarDado(){
-        int result = (int) ((Math.random() * (5)) + 1);
-        return result;
+    public int jogarDado() {
+        RandomGenerator randomGenerator = new Random();
+        return randomGenerator.nextInt(6);
     }
 
     //Jogador é um byte (0 representa um jogador e 1 o outro)
-    public void jogar(int jogador){
-        Scanner in = new Scanner(System.in); 
-        
+    public void jogar(int jogador) {
+        Scanner in = new Scanner(System.in);
+
         int dado = jogarDado();
         System.out.println("Resultado do dado: " + dado);
         System.out.print("Qual peao gostaria de mover? " + (jogador == 0 ? "[0-3]" : "[4-7]"));
 
         int index = in.nextInt(); //index do peao desejado no array de peoes 
-        if(checarPosicao(peoes.get(index), dado))
-            System.out.println("Peão movido, nova posição: " + peoes.get(index).getPosicao());
-        else 
-            System.out.println("Um peão inimigo está nessa casa, nova posição: " + peoes.get(index).getPosicao());
+        Peao curr = peoes.get(index);
+        if (checarPosicao(curr, dado)) {
+            System.out.println("Peao movido, nova posicao: " + (curr.getPosicao() - curr.posicaoInicial));
+        } else {
+            System.out.println("Um peao inimigo está nessa casa, nova posicao: " + (curr.getPosicao() - curr.posicaoInicial));
+        }
     }
-    
+
     //Checa se um peao pode ser movido para a nova posição
-    public boolean checarPosicao(Peao p, int dado){
+    public boolean checarPosicao(Peao p, int dado) {
         int novaPosicao = p.getPosicao() + dado;
         Peao peaoNovaPosicao = tabuleiro.get(novaPosicao).getPeao();
         //Já existe um peão nesse quadrado do tabuleiro, e ele tem a cor diferente
         //ou seja, é do inimigo, logo o peao p deve retornar a casa incial
-        if(peaoNovaPosicao != null &&  peaoNovaPosicao.getCor() != p.getCor()){ 
+        if (peaoNovaPosicao != null && peaoNovaPosicao.getCor() != p.getCor()) {
             p.setPosicao(p.posicaoInicial);
             tabuleiro.get(p.posicaoInicial).setPeao(p);
             return false;
