d85cfbbf1d05dda5af4e62bbf912efc0434a5e5c
karol marques <karoldm1579@gmail.com>
2023-03-25 16:44:03 -0300

    ajuster

--- src/classes/Jogo.java
+++ src/classes/Jogo.java
@@ -28,20 +28,21 @@ public class Jogo {
         this.peoes.add(new Peao((byte) 1, 14));
         this.peoes.add(new Peao((byte) 1, 14));
 
-        //Iniciando tabuleiro
+        //Iniciando tabuleiro com 51 casas
         for (int i = 0; i < 51; i++) {
-            tabuleiro.add(new Square(null));
+            tabuleiro.add(new Square());
         }
 
-        tabuleiro.get(0).setPeao(peoes.get(0));
-        tabuleiro.get(0).setPeao(peoes.get(1));
-        tabuleiro.get(0).setPeao(peoes.get(2));
-        tabuleiro.get(0).setPeao(peoes.get(3));
+        //Alocando peões nas suas posições iniciais do tabuleiro
+        tabuleiro.get(0).addPeao(peoes.get(0));
+        tabuleiro.get(0).addPeao(peoes.get(1));
+        tabuleiro.get(0).addPeao(peoes.get(2));
+        tabuleiro.get(0).addPeao(peoes.get(3));
 
-        tabuleiro.get(14).setPeao(peoes.get(4));
-        tabuleiro.get(14).setPeao(peoes.get(5));
-        tabuleiro.get(14).setPeao(peoes.get(6));
-        tabuleiro.get(14).setPeao(peoes.get(7));
+        tabuleiro.get(14).addPeao(peoes.get(4));
+        tabuleiro.get(14).addPeao(peoes.get(5));
+        tabuleiro.get(14).addPeao(peoes.get(6));
+        tabuleiro.get(14).addPeao(peoes.get(7));
 
     }
 
@@ -67,20 +68,29 @@ public class Jogo {
             System.out.println("Um peao inimigo esta nessa casa, nova posicao: " + curr.getPosicao());
         }
     }
+    
+    public void move(Peao p, int novaPosicao){
+        tabuleiro.get(p.getPosicao()).remove(p); //remove o peao da casa atual
+        tabuleiro.get(novaPosicao).addPeao(p); //adiciona o peao a nova casa
+        p.setPosicao(novaPosicao); //atualiza a posicao do peao
+    }
 
     //Checa se um peao pode ser movido para a nova posição
     public boolean checarPosicao(Peao p, int dado) {
         int novaPosicao = p.getPosicao() + dado;
-        Peao peaoNovaPosicao = tabuleiro.get(novaPosicao).getPeao();
+        ArrayList<Peao> peoesNovaPosicao = tabuleiro.get(novaPosicao).getPeoes();
         //Já existe um peão nesse quadrado do tabuleiro, e ele tem a cor diferente
         //ou seja, é do inimigo, logo o peao p deve retornar a casa incial
-        if (peaoNovaPosicao != null && peaoNovaPosicao.getCor() != p.getCor()) {
-            p.setPosicao(p.posicaoInicial);
-            tabuleiro.get(p.posicaoInicial).setPeao(p);
-            return false;
+        if (!peoesNovaPosicao.isEmpty()) {
+            for (Peao pNovaPosicao : peoesNovaPosicao) {
+                if (pNovaPosicao.getCor() != p.getCor()) {
+                    this.move(p, p.posicaoInicial);
+                    return false;
+                }
+            }
         }
-        p.setPosicao(novaPosicao); //jogada válida, peao move para nova posicao
-        tabuleiro.get(p.getPosicao() + dado).setPeao(p);
+        //Se o movimento é válido o peao é movido para a nova casa
+        this.move(p, novaPosicao); 
         return true;
     }
 }
--- src/classes/Square.java
+++ src/classes/Square.java
@@ -1,22 +1,29 @@
 
 package classes;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Karol
  */
 public class Square {
-    private Peao peao;
+    //Peões ocupando esse quadrado do tabuleiro
+    //Mais de um peao pode estar nessa casa se eles forem do mesmo jogador
+    private ArrayList<Peao> peoes = new ArrayList<>();
 
-    public Square(Peao peao){
-      this.peao = peao;
+    public Square(){
     }
     
-    public void setPeao(Peao peao){
-      this.peao = peao;
+    public void addPeao(Peao peao){
+      this.peoes.add(peao);
     }
 
-    public Peao getPeao(){
-      return this.peao;
+    public ArrayList<Peao> getPeoes(){
+      return this.peoes;
+    }
+    
+    public void remove(Peao p){
+        this.peoes.remove(p); 
     }
 }
