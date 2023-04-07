053777cbb5dd47512206259e15c32ba29751fdad
karol marques <karoldm1579@gmail.com>
2023-04-07 17:53:58 -0300

    desenhando tabuleiro

--- src/views/UIJogo.java
+++ src/views/UIJogo.java
@@ -1,20 +1,127 @@
-
 package views;
 
 import classes.Jogo;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridLayout;
+import java.awt.Image;
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.border.Border;
 
 /**
  *
  * @author Karol
  */
 public class UIJogo extends javax.swing.JFrame {
+
     private Jogo jogo = new Jogo();
     private int jogador = 0;
+    private JButton[][] squares = new JButton[15][15];
+
     /**
      * Creates new form NewJFrame
      */
     public UIJogo() {
         initComponents();
+
+        generateLudoBoard();
+    }
+
+    private void generateLudoBoard() {
+        boardGame.setSize(600, 600);
+        boardGame.setLayout(new GridLayout(15, 15));
+
+        Border lineBorder = BorderFactory.createLineBorder(Color.black);
+
+        // Criando tabuleiro 15x15
+        // O grid layout do java cria uma tabela da seguinte forma: 
+        // |2,0  2,1  2,2|
+        // |1,0  1,1  1,2|
+        // |0,0  0,1  0,2|
+        for (int i = 0; i < 15; i++) {
+            for (int j = 0; j < 15; j++) {
+                JButton squareButton = new JButton();
+                squareButton.setPreferredSize(new Dimension(40, 40));
+                squareButton.setBackground(Color.WHITE);
+                squareButton.setBorder(lineBorder);
+                squareButton.setEnabled(false);
+                boardGame.add(squareButton, i, j);
+                squares[i][j] = squareButton;
+            }
+        }
+
+        // Criando os quatro quadrados coloridos dos cantos
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                squares[i][j].setBackground(Color.BLUE);
+            }
+        }
+        for (int i = 9; i < 15; i++) {
+            for (int j = 0; j < 6; j++) {
+                squares[i][j].setBackground(Color.YELLOW);
+            }
+        }
+        for (int i = 0; i < 6; i++) {
+            for (int j = 9; j < 15; j++) {
+                squares[i][j].setBackground(Color.RED);
+            }
+        }
+        for (int i = 9; i < 15; i++) {
+            for (int j = 9; j < 15; j++) {
+                squares[i][j].setBackground(Color.GREEN);
+            }
+        }
+
+        // Desenhando trilhas
+        squares[8][1].setBackground(Color.YELLOW);
+        squares[7][1].setBackground(Color.YELLOW);
+
+        for (int i = 1; i < 6; i++) {
+            squares[7][i].setBackground(Color.YELLOW);
+        }
+
+        squares[6][13].setBackground(Color.RED);
+        squares[7][13].setBackground(Color.RED);
+
+        for (int i = 9; i < 13; i++) {
+            squares[7][i].setBackground(Color.RED);
+        }
+
+        squares[1][6].setBackground(Color.BLUE);
+        squares[1][7].setBackground(Color.BLUE);
+
+        for (int i = 2; i < 6; i++) {
+            squares[i][7].setBackground(Color.BLUE);
+        }
+
+        squares[13][8].setBackground(Color.GREEN);
+        squares[13][7].setBackground(Color.GREEN);
+
+        for (int i = 9; i < 13; i++) {
+            squares[i][7].setBackground(Color.GREEN);
+        }
+
+        // Desenhando quadrado central (chegada)
+        for (int i = 6; i < 9; i++) {
+            for (int j = 6; j < 9; j++) {
+                squares[i][j].setBackground(Color.BLACK);
+            }
+        }
+
+        try {
+            Image img = ImageIO.read(getClass().getResource("/assets/goal.bmp"));
+            Image newImage = img.getScaledInstance(30, 25, Image.SCALE_DEFAULT);
+            squares[7][7].setIcon(new ImageIcon(newImage));
+        } catch (Exception ex) {
+            System.out.println(ex);
+            JOptionPane.showMessageDialog(this, "Erro ao criar tabuleiro!", "Erro", JOptionPane.ERROR_MESSAGE);
+            this.dispose();
+        }
+
     }
 
     /**
@@ -146,9 +253,9 @@ public class UIJogo extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void menuSerHostActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuSerHostActionPerformed
-       IP hostIP = new IP();
-       hostIP.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-       hostIP.setVisible(true);
+        IP hostIP = new IP();
+        hostIP.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        hostIP.setVisible(true);
     }//GEN-LAST:event_menuSerHostActionPerformed
 
     private void menuConectarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_menuConectarActionPerformed
