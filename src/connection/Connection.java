72cd77b667004e261149f36e4091fbb5c8553115
Willian Murayama <willianyos13@gmail.com>
2023-04-09 17:11:22 -0300

    Created connection and host files

---
+++ src/connection/Connection.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package connection;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Willian
+ */
+public class Connection implements Runnable {
+//Runnable implementa as threads, que sao necessarias para execucao do jogo
+
+    public void run() {
+
+    }
+}
