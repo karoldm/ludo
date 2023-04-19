396299ceee48bb34fbf98fdabfc5d293f325296d
Willian Murayama <willianyos13@gmail.com>
2023-04-19 12:05:35 -0300

    Fixed controller variable on interface

--- ludoMaven/src/main/java/views/Connect.java
+++ ludoMaven/src/main/java/views/Connect.java
@@ -20,7 +20,7 @@ public class Connect extends javax.swing.JFrame {
      * Creates new form ConnectIP
      */
 //    Isso vai dar problema
-    private static ControladorJogo controller;
+    private ControladorJogo controller;
 
     public Connect(ControladorJogo controller) {
         this.controller = controller;
@@ -113,7 +113,7 @@ public class Connect extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    public static void main(String args[], ControladorJogo controller) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
--- ludoMaven/src/main/java/views/Host.java
+++ ludoMaven/src/main/java/views/Host.java
@@ -15,7 +15,7 @@ import javax.swing.JOptionPane;
  */
 public class Host extends javax.swing.JFrame {
 
-    private static ControladorJogo controller;
+    private ControladorJogo controller;
 
     /**
      * Creates new form IP
@@ -92,7 +92,7 @@ public class Host extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    public static void main(String args[], ControladorJogo controller) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
